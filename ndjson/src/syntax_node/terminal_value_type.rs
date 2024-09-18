3ebc5c0ca50a16e98f5fdacaa7cfa3342c1f8f2c
tokeiya3 <netseed@seedcreate.onmicrosoft.com>
2024-09-18 17:12:17 +0900

    Add test helper methods to TerminalNodeType
    Introduced assert methods for Boolean, Number, String, and Null types within a test_helper module. These methods facilitate more straightforward testing by providing convenient assertions on TerminalNodeType variants.

--- ndjson/src/syntax_node/terminal_value_type.rs
+++ ndjson/src/syntax_node/terminal_value_type.rs
@@ -4,3 +4,33 @@ pub enum TerminalNodeType{
 	String,
 	Null
 }
+
+#[cfg(test)]
+pub mod test_helper{
+	use super::*;
+
+	#[cfg(test)]
+	
+	impl TerminalNodeType{
+		#[cfg(test)]
+		
+		pub fn assert_boolean(&self){
+			assert!(matches!(self,TerminalNodeType::Boolean));
+		}
+		#[cfg(test)]
+		
+		pub fn assert_number(&self){
+			assert!(matches!(self,TerminalNodeType::Number));
+		}
+		#[cfg(test)]
+		
+		pub fn assert_string(&self){
+			assert!(matches!(self,TerminalNodeType::String));
+		}
+		#[cfg(test)]
+		
+		pub fn assert_null(&self){
+			assert!(matches!(self,TerminalNodeType::Null));
+		}
+	}
+}
