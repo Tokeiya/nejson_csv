414362719dac248a96e9bd685b96636707b1f010
tokeiya3 <netseed@seedcreate.onmicrosoft.com>
2024-09-23 16:15:46 +0900

    Implement object parser tests for various data types
    Added comprehensive tests for the object parser to validate its ability to parse different JSON data types, including strings, integers, nulls, boolean values, and floats. Also included tests to verify correct parsing of empty objects with and without whitespace.

--- ndjson/src/parser/object.rs
+++ ndjson/src/parser/object.rs
@@ -27,11 +27,66 @@ mod test {
 
 	#[test]
 	fn object() {
-		todo!()
+		let str = generate(vec![
+			("rust", r#""rust""#),
+			("int", "42"),
+			("null", "null"),
+			("true", "true"),
+			("false", "false"),
+			("float", "42.195"),
+		]);
+		let mut parser = super::object::<&str>();
+
+		let (act, rem) = parser.parse(&str).unwrap();
+		assert_eq!(rem, "");
+
+		let act = act.value().extract_contents();
+		assert_eq!(act.len(), 6);
+
+		let piv = &act[0];
+		piv.assert_lead_trail(None, None);
+		piv.key().assert_string("rust");
+		piv.value().extract_terminal().assert_string("rust");
+
+		let piv = &act[1];
+		piv.assert_lead_trail(None, None);
+		piv.key().assert_string("int");
+		piv.value().extract_terminal().assert_integer("42");
+
+		let piv = &act[2];
+		piv.assert_lead_trail(None, None);
+		piv.key().assert_string("null");
+		piv.value().extract_terminal().assert_null();
+
+		let piv = &act[3];
+		piv.assert_lead_trail(None, None);
+		piv.key().assert_string("true");
+		piv.value().extract_terminal().assert_true();
+
+		let piv = &act[4];
+		piv.assert_lead_trail(None, None);
+		piv.key().assert_string("false");
+		piv.value().extract_terminal().assert_false();
+
+		let piv = &act[5];
+		piv.assert_lead_trail(None, None);
+		piv.key().assert_string("float");
+		piv.value().extract_terminal().assert_float("42.195");
 	}
 
 	#[test]
 	fn empty() {
-		todo!();
+		let str = "{}";
+		let mut parser = super::object::<&str>();
+
+		let (act, rem) = parser.parse(&str).unwrap();
+		assert_eq!(rem, "");
+		act.value().assert_empty("");
+
+		let str = format!("{{{WS}}}");
+		let mut parser = super::object::<&str>();
+		let (act, rem) = parser.parse(&str).unwrap();
+		assert_eq!(rem, "");
+		act.value().assert_empty(WS);
 	}
 }
