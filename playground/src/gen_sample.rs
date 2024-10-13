797b62f49a972ede17ddc2e7a8ea3b3c6e400184
Tokeiya <netseed@seedcreate.onmicrosoft.com>
2024-10-13 15:11:05 +0900

    sync

--- playground/src/gen_sample.rs
+++ playground/src/gen_sample.rs
@@ -60,10 +60,9 @@ fn generate_object(
 	current_depth: usize,
 	threshold_depth: usize,
 	recent: RecentNode,
+	indent_cnt: usize,
 ) -> IoResult<()> {
-	let indent = std::iter::repeat(" ")
-		.take(current_depth * INDENT)
-		.collect::<String>();
+	let indent = std::iter::repeat(" ").take(indent_cnt).collect::<String>();
 	let str = indent.as_str();
 
 	writer.write_fmt(format_args_nl!(
@@ -74,7 +73,7 @@ fn generate_object(
 	let keys = choice_keys(rnd, size);
 
 	for (idx, key) in keys.iter().enumerate() {
-		writer.write_fmt(format_args!(r#"{str}"{key}":"#))?;
+		writer.write_fmt(format_args!(r#" {indent}"{key}":"#))?;
 		generate_node(
 			writer,
 			rnd,
@@ -82,6 +81,7 @@ fn generate_object(
 			current_depth + 1,
 			threshold_depth,
 			RecentNode::Object,
+			indent_cnt + INDENT + 1,
 		)?;
 
 		if size - 1 > idx {
@@ -89,7 +89,7 @@ fn generate_object(
 		}
 	}
 
-	writer.write_fmt(format_args!("\n{str}}}"))?;
+	writer.write_fmt(format_args!("\n{indent}}}"))?;
 
 	Ok(())
 }
@@ -101,10 +101,9 @@ fn generate_array(
 	current_depth: usize,
 	threshold_depth: usize,
 	recent: RecentNode,
+	indent_cnt: usize,
 ) -> IoResult<()> {
-	let indent = std::iter::repeat(" ")
-		.take(current_depth * INDENT)
-		.collect::<String>();
+	let indent = std::iter::repeat(" ").take(indent_cnt).collect::<String>();
 
 	let str = indent.as_str();
 
@@ -120,6 +119,7 @@ fn generate_array(
 			current_depth + 1,
 			threshold_depth,
 			RecentNode::Array,
+			indent_cnt + INDENT,
 		)?;
 
 		if size - 1 > idx {
@@ -138,10 +138,9 @@ fn generate_node(
 	current_depth: usize,
 	threshold_depth: usize,
 	recent: RecentNode,
+	indent_cnt: usize,
 ) -> IoResult<()> {
-	let indent = std::iter::repeat(" ")
-		.take(current_depth * INDENT)
-		.collect::<String>();
+	let indent = std::iter::repeat(" ").take(indent_cnt).collect::<String>();
 
 	if current_depth >= threshold_depth {
 		writer.write_fmt(format_args!(
@@ -153,12 +152,24 @@ fn generate_node(
 		Ok(())
 	} else {
 		match choice_node(rnd) {
-			NodeTypes::Array => {
-				generate_array(writer, rnd, cnt, current_depth + 1, threshold_depth, recent)
-			}
-			NodeTypes::Object => {
-				generate_object(writer, rnd, cnt, current_depth + 1, threshold_depth, recent)
-			}
+			NodeTypes::Array => generate_array(
+				writer,
+				rnd,
+				cnt,
+				current_depth + 1,
+				threshold_depth,
+				recent,
+				indent_cnt + INDENT,
+			),
+			NodeTypes::Object => generate_object(
+				writer,
+				rnd,
+				cnt,
+				current_depth + 1,
+				threshold_depth,
+				recent,
+				indent_cnt + INDENT,
+			),
 			NodeTypes::Terminal => {
 				writer.write_fmt(format_args!(
 					"{}{}",
@@ -174,7 +185,15 @@ fn generate_node(
 
 pub fn generate_sample(writer: &mut impl Write, depth: usize) -> IoResult<()> {
 	let mut rnd = rand::thread_rng();
-	generate_node(writer, &mut rnd, &Cell::new(0), 0, depth, RecentNode::Root)?;
+	generate_node(
+		writer,
+		&mut rnd,
+		&Cell::new(0),
+		0,
+		depth,
+		RecentNode::Root,
+		0,
+	)?;
 
 	Ok(())
 }
--- playground/src/main.rs
+++ playground/src/main.rs
@@ -3,10 +3,43 @@
 mod cache_search;
 mod gen_sample;
 
+use combine::Parser;
+use ndjson::parser::value_parser;
+use ndjson::syntax_node::prelude::*;
+use std::cell::Ref;
+use std::env;
 use std::fs::File;
-use std::io::{BufWriter, Write};
+use std::io::{BufReader, BufWriter, Read, Write};
+use std::path::PathBuf;
 
 fn main() {
+	gen_sample();
+
+	let current_dir: PathBuf = env::current_dir().unwrap();
+	println!("The current directory is: {:?}", current_dir);
+
+	let mut file = File::open("./artifact/hoge.json").unwrap();
+	let mut rdr = BufReader::new(&mut file);
+
+	let mut buff = String::new();
+	_ = rdr.read_to_string(&mut buff).unwrap();
+
+	let mut parser = value_parser::<&str>();
+
+	let (root, _) = parser.parse(&buff).unwrap();
+	root.set_identity(Identity::Root);
+
+	let iter = BreadthFirstIterator::new(root.clone());
+
+	for elem in iter {
+		match elem.value() {
+			NodeValue::Terminal(t) => 
+			_=>continue
+		}
+	}
+}
+
+fn gen_sample() {
 	let mut file = File::create("./artifact/hoge.json").unwrap();
 	let mut writer = BufWriter::new(&mut file);
 
