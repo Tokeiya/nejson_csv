a5b664a1135aac1d434431cf72b09386eeb19c08
tokeiya3@notebook <netseed@seedcreate.onmicrosoft.com>
2024-10-18 13:51:33 +0900

    wip

--- ndjson/src/parser/log.rs
+++
@@ -1,3 +0,0 @@
-mod local_timestamper;
-mod log_element;
-mod time_stamper;
--- ndjson/src/parser/log/time_stamper.rs
+++
@@ -1,5 +0,0 @@
-use chrono::{DateTime, TimeZone};
-
-pub trait TimeStamper<Tz: TimeZone> {
-	fn time_stamp() -> DateTime<Tz>;
-}
