87b6fc917701a4d54027499bb2973a2fcf0465c2
tokeiya3 <netseed@seedcreate.onmicrosoft.com>
2024-10-21 17:35:13 +0900

    Refactor code and cleanup dependencies
    Add test-only attribute to some test helpers and remove unnecessary dependencies from Cargo.lock and Cargo.toml. Also update coverage toolchain checks for nightly-x86_64-pc-windows-gnu.

--- ndjson/src/test_helper/mock_time_stamper.rs
+++ ndjson/src/test_helper/mock_time_stamper.rs
@@ -2,15 +2,20 @@ use crate::log::prelude::TimeStamper;
 use chrono::{DateTime, Local};
 use std::sync::atomic::AtomicUsize;
 use std::sync::LazyLock;
+
+#[cfg(test)]
 pub static EXPECTED: LazyLock<DateTime<Local>> = LazyLock::new(|| {
 	std::thread::sleep(std::time::Duration::from_micros(100));
 	Local::now()
 });
 
+#[cfg(test)]
 pub static CALL_COUNT: AtomicUsize = AtomicUsize::new(0usize);
 
+#[cfg(test)]
 pub struct MockTimeStamper;
 
+#[cfg(test)]
 impl TimeStamper<Local> for MockTimeStamper {
 	fn time_stamp() -> DateTime<Local> {
 		CALL_COUNT.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
